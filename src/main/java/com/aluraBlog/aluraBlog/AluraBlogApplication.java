b186f9fdf74bf408cd8ef20634d10dfa9d5127ce
Jose Lambrechts <alt165@gmail.com>
2025-01-15 15:39:09 -0300

    crear estructura inicial del proyecto

---
+++ src/main/java/com/aluraBlog/aluraBlog/AluraBlogApplication.java
@@ -0,0 +1,13 @@
+package com.aluraBlog.aluraBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluraBlog/aluraBlog/AluraBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluraBlog.aluraBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
